58cf1613b7ee69dbdf5f31bec9b122326d68acaa
seedevk8s <seedevk8s@gmail.com>
2024-09-26 21:00:59 +0900

    db 설정 부분 수정

--- src/main/java/com/kosta/hboot/repository/HouseRepository.java
+++ src/main/java/com/kosta/hboot/repository/HouseRepository.java
@@ -1,8 +1,9 @@
 package com.kosta.hboot.repository;
 
+import com.kosta.hboot.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HouseRepository extends JpaRepository {
+public interface HouseRepository extends JpaRepository<Member,Long> {
 }
